ad2cb544a3c6f6ff9f5bd8a4bf44ce5cc30aeb81
Abi <elabi3@gmail.com>
2020-04-21 01:07:15 +0200

    feat: update text to speech api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,8 @@ import {
     IonList,
     IonListHeader,
     IonPage,
+    IonSegment,
+    IonSegmentButton,
     IonTextarea,
     IonTitle,
     IonToolbar
@@ -15,18 +17,18 @@ import {
 import React, {useState} from 'react';
 import './Home.css';
 
-const axios = require('axios');
-const FileDownload = require('js-file-download');
+const FileSaver = require('file-saver');
 
 const Home: React.FC = () => {
-    const [spanish, setSpanish] = useState<string>();
-    const [english, setEnglish] = useState<string>();
+    const [text, setText] = useState<string>();
+    const [language, setLanguage] = useState<string>('es-es');
     const [fileName, setFileName] = useState<string>();
 
-    axios.get('https://translate.google.com.vn/translate_tts?ie=UTF-8&q=prueba+hola&tl=es&client=tw-ob')
-        .then((response: any) => {
-            FileDownload(response.data, 'report.mpga');
-        });
+    const download = (text: string, lang: string, fileName: string) => {
+        const replacedText = text.replace(/\s/g, '%20');
+        const url = `http://api.voicerss.org/?key=f8538537192c43bcb28d89955f9e5dfc&hl=${lang}&src=${replacedText}&c=mp3`;
+        FileSaver.saveAs(url, `${fileName ? fileName : replacedText}.mp3`);
+    };
 
     return (
         <IonPage>
@@ -41,12 +43,8 @@ const Home: React.FC = () => {
                         <IonLabel>Introduzca texto</IonLabel>
                     </IonListHeader>
                     <IonItem>
-                        <IonLabel position="floating">Español</IonLabel>
-                        <IonTextarea rows={8} value={spanish} onIonChange={e => setSpanish(e.detail.value!)}/>
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel position="floating">Inglés</IonLabel>
-                        <IonTextarea rows={8} value={english} onIonChange={e => setEnglish(e.detail.value!)}/>
+                        <IonLabel position="floating">Texto</IonLabel>
+                        <IonTextarea rows={8} value={text} onIonChange={e => setText(e.detail.value!)}/>
                     </IonItem>
                     <IonListHeader>
                         <IonLabel>Descarga ficheros</IonLabel>
@@ -56,18 +54,21 @@ const Home: React.FC = () => {
                         <IonInput value={fileName} placeholder="Si no seleccionas uno se autogenera"
                                   onIonChange={e => setFileName(e.detail.value!)}/>
                     </IonItem>
-                    <IonButton onClick={() => {
-                    }}>
-                        Español
-                    </IonButton>
-                    <IonButton onClick={() => {
-                    }}>
-                        Inglés
-                    </IonButton>
-                    <IonButton onClick={() => {
-                    }}>
-                        Ambos
-                    </IonButton>
+                    <IonSegment value={language} onIonChange={e => setLanguage(e.detail.value as string)}>
+                        <IonSegmentButton value="es-es">
+                            <IonLabel>Español</IonLabel>
+                        </IonSegmentButton>
+                        <IonSegmentButton value="en-us">
+                            <IonLabel>Inglés</IonLabel>
+                        </IonSegmentButton>
+                    </IonSegment>
+                    <div className="buttonContainer">
+                        <IonButton disabled={text === undefined || text === ''} onClick={() => {
+                            download(text as string, language as string, fileName as string);
+                        }}>
+                            Descargar
+                        </IonButton>
+                    </div>
                 </IonList>
 
             </IonContent>
